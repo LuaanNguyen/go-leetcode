94cc55c5c68556d7b55e361ab110b8c03c4b0b24
LuaanNguyen <luanthiennguyen2003@gmail.com>
2025-01-07 10:38:16 -0700

    Add test for isSubsequence

--- easy/0392_is_subsequence/0392_is_subsequence_test.go
+++ easy/0392_is_subsequence/0392_is_subsequence_test.go
@@ -1,2 +1,33 @@
 package is_subsequence
 
+import "testing"
+
+func TestIsSubsequence(t *testing.T) {
+	tests := []struct {
+		s	string
+		t	string
+		expectedResult 	bool 
+	} {
+		{
+			"abc",
+			"ahbgdc",
+			true,
+		},
+		{
+			"axc",
+			"ahbgdc",
+			false,	
+		},
+	}
+
+	//Loop through test cases
+	for _, test := range tests {
+		t.Run("test", func(t *testing.T) {
+			got := isSubsequence(test.s, test.t)
+
+			if got != test.expectedResult {
+				t.Errorf("For input s = %s and t = %s, expected result %t, but got %t", test.s, test.t, test.expectedResult, got)
+			} 
+		})
+	}
+}
