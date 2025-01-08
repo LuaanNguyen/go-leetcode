48530aa78dda401dc02a0e853bf343c1e96567e6
LuaanNguyen <luanthiennguyen2003@gmail.com>
2025-01-07 23:01:34 -0700

    Added example test cases for leetcode 3042

--- easy/3042_count_prefix_and_sufix_pairs_I/3042_count_prefix_and_sufix_pairs_I.go
+++ easy/3042_count_prefix_and_sufix_pairs_I/3042_count_prefix_and_sufix_pairs_I.go
@@ -1,1 +1,21 @@
 package count_prefix_and_sufix_pairs_I
+
+import "strings"
+
+func countPrefixSuffixPairs(words []string) int {
+    res := 0 
+
+    for i := 0; i < len(words); i++ {
+        for j := i + 1; j < len(words); j++ {
+            if isPrefixAndSuffix(words[i], words[j]) {
+                res++
+            }
+        }
+    }
+
+    return res
+}
+
+func isPrefixAndSuffix(str1 string, str2 string) bool {
+    return strings.HasPrefix(str2, str1) && strings.HasSuffix(str2, str1)
+}
--- easy/3042_count_prefix_and_sufix_pairs_I/3042_count_prefix_and_sufix_pairs_I_test.go
+++ easy/3042_count_prefix_and_sufix_pairs_I/3042_count_prefix_and_sufix_pairs_I_test.go
@@ -1,1 +1,34 @@
 package count_prefix_and_sufix_pairs_I
+
+import (
+	"testing"
+)
+
+func TestCountPrefixSuffixPairs(t *testing.T) {
+    tests := []struct {
+        name     string
+        words    []string
+        expected int
+    }{
+        {
+            name:     "Test case 1",
+            words:    []string{"pa","papa","ma","mama"},
+            expected: 2, 
+        },
+		{
+            name:     "Test case 2",
+            words:    []string{"abab","ab"},
+            expected: 0, 
+        },
+       
+    }
+
+    for _, tt := range tests {
+        t.Run(tt.name, func(t *testing.T) {
+            got := countPrefixSuffixPairs(tt.words)
+            if got != tt.expected {
+                t.Errorf("countPrefixSuffixPairs(%v) = %v; want %v", tt.words, got, tt.expected)
+            }
+        })
+    }
+}
