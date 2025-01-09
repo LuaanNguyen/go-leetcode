beff6e2d349960b8a95516c525ab1c2498e9a61a
LuaanNguyen <luanthiennguyen2003@gmail.com>
2025-01-08 21:41:50 -0700

    Completed daily 01/08/2024 - 2185. Counting Words With a Given Prefix

--- easy/2185_counting_words_with_a_given_string/2185_counting_words_with_a_given_string.go
+++ easy/2185_counting_words_with_a_given_string/2185_counting_words_with_a_given_string.go
@@ -1,2 +1,15 @@
 package counting_words_with_a_given_string
 
+import "strings"
+
+func prefixCount(words []string, pref string) int {
+	count := 0 
+
+	for i := 0; i < len(words); i ++ {
+		if strings.HasPrefix(words[i], pref) {
+			count++
+		}
+	}
+
+	return count
+}
--- easy/2185_counting_words_with_a_given_string/2185_counting_words_with_a_given_string_test.go
+++ easy/2185_counting_words_with_a_given_string/2185_counting_words_with_a_given_string_test.go
@@ -1,1 +1,33 @@
 package counting_words_with_a_given_string
+
+import "testing"
+
+func TestPrefixCount(t *testing.T) {
+	tests := []struct{
+		words []string
+		pref  string 
+		expectedResult int
+	}{
+		{
+			words:          []string{"pay", "attention", "practice", "attend"},
+			pref:           "at",
+			expectedResult: 2,
+		},
+		{
+			words:          []string{"leetcode","win","loops","success"},
+			pref:           "code",
+			expectedResult: 0,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run("test", func(t *testing.T) {
+            got := prefixCount(test.words, test.pref)
+            if got != test.expectedResult {
+				t.Errorf("expected %d, but got %d", test.expectedResult, got)
+            }
+        })
+
+	}
+
+}	
