dcade340ad98d804949c22569985b0d4560a0d10
LuaanNguyen <luanthiennguyen2003@gmail.com>
2025-01-12 16:07:21 -0700

    Completed 2116. Check if a Parentheses String Can Be Valid

--- medium/2116_check_if_a_parentheses_string_can_be_valid/2116_check_if_a_parentheses_string_can_be_valid.go
+++ medium/2116_check_if_a_parentheses_string_can_be_valid/2116_check_if_a_parentheses_string_can_be_valid.go
@@ -1,1 +1,38 @@
 package check_if_a_parentheses_string_can_be_valid
+
+func canBeValid(s string, locked string) bool {
+    if len(s) % 2 == 1 {
+        return false 
+    }
+
+    openBrackets := make([]int, 0)
+    unlocked := make([]int, 0)
+
+    for i, _ := range s {
+        if locked[i] == '0' {
+            unlocked = append(unlocked, i)
+        } else if s[i] == '(' {
+            openBrackets = append(openBrackets, i)
+        } else if s[i] == ')' {
+            if len(openBrackets) > 0 {
+                openBrackets = openBrackets[:len(openBrackets) - 1]
+            } else if len(unlocked) > 0 {
+                unlocked = unlocked[:len(unlocked) - 1]
+            } else {
+                return false
+            }
+        }
+    }
+
+    for {
+        if len(openBrackets) > 0 && len(unlocked) > 0 && openBrackets[len(openBrackets) - 1] < unlocked[len(unlocked) - 1] {
+            openBrackets = openBrackets[:len(openBrackets) - 1]
+            unlocked = unlocked[:len(unlocked) - 1]
+        } else {
+            break
+        }
+    }
+
+    return len(openBrackets) == 0 
+    
+}
--- medium/2116_check_if_a_parentheses_string_can_be_valid/2116_check_if_a_parentheses_string_can_be_valid_test.go
+++ medium/2116_check_if_a_parentheses_string_can_be_valid/2116_check_if_a_parentheses_string_can_be_valid_test.go
@@ -1,1 +1,35 @@
 package check_if_a_parentheses_string_can_be_valid
+
+import "testing"
+
+
+func TestCanBeValid(t *testing.T) {
+	tests := []struct {
+		name	string
+		s 	string 
+		locked	string 
+		expectedResult	bool
+	} {
+		{
+			name: "Test Case 1",
+			s: "))()))",
+			locked: "010100",
+			expectedResult: true,
+		},
+		{
+			name: "Test Case 2",
+			s: "()()",
+			locked: "010100",
+			expectedResult: true,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func (t * testing.T) {
+			got := canBeValid(test.s, test.locked)
+			if got != test.expectedResult {
+				t.Errorf("canBeValidConstruct(\"%s\", %s) = %t; want %t", test.s, test.locked, got, test.expectedResult)
+			}
+	})
+	}
+}
