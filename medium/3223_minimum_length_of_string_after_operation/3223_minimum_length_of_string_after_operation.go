79c83c54c003af3bbbb87f408127350fb94ff6ed
LuaanNguyen <luanthiennguyen2003@gmail.com>
2025-01-13 14:31:07 -0700

    Add problem 3223. Minimum Length of String After Operations

--- medium/3223_minimum_length_of_string_after_operation/3223_minimum_length_of_string_after_operation.go
+++ medium/3223_minimum_length_of_string_after_operation/3223_minimum_length_of_string_after_operation.go
@@ -1,1 +1,22 @@
 package minimum_length_of_string_after_operation
+
+func minimumLength(s string) int {
+    frequencyTable := make(map[rune]int)
+
+    //Count the frequency of each item in the table 
+    for _, c := range s {
+        frequencyTable[c]++
+    }
+
+    //Calculate the number of character to delete
+    count := 0
+    for _, val := range frequencyTable {
+        if val % 2 == 1 {
+            count += val - 1
+        } else {
+            count += val - 2
+        }
+    }
+
+    return len(s) - count
+}
--- medium/3223_minimum_length_of_string_after_operation/3223_minimum_length_of_string_after_operation_test.go
+++ medium/3223_minimum_length_of_string_after_operation/3223_minimum_length_of_string_after_operation_test.go
@@ -1,1 +1,29 @@
 package minimum_length_of_string_after_operation
+
+import "testing"
+
+
+func TestMinimumLength(t *testing.T) {
+	test := []struct {
+		name	string
+		s	string
+		expectedResult int 
+	} {
+		{
+			name: "Test Case 1",
+			s: "abaacbcbb",
+			expectedResult: 5,
+		},
+		{
+			name: "Test Case 2",
+			s: "aa",
+			expectedResult: 2,
+		},
+	}
+
+	for _, test := range tests {
+
+
+	}
+
+}
