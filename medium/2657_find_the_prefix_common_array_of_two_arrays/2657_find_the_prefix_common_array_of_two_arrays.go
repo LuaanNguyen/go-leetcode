c30557859cddf6ce9b2878ae92f130f7cabc64d4
LuaanNguyen <luanthiennguyen2003@gmail.com>
2025-01-14 11:42:48 -0700

    Completed 2657. Find the Prefix Common Array of Two Arrays

--- medium/2657_find_the_prefix_common_array_of_two_arrays/2657_find_the_prefix_common_array_of_two_arrays.go
+++ medium/2657_find_the_prefix_common_array_of_two_arrays/2657_find_the_prefix_common_array_of_two_arrays.go
@@ -1,1 +1,20 @@
 package find_the_prefix_common_array_of_two_arrays
+
+func findThePrefixCommonArray(A []int, B []int) []int {
+	A_count := make(map[int]bool)
+	B_count := make(map[int]bool)
+
+	res := make([]int, len(A))
+
+	for i := 0; i < len(A); i ++ {
+		A_count[A[i]] = true
+		B_count[B[i]] = true
+
+		for key := range A_count {
+			if B_count[key] {
+				res[i]++
+			}
+		}
+	}
+	return res 
+}
--- medium/2657_find_the_prefix_common_array_of_two_arrays/2657_find_the_prefix_common_array_of_two_arrays_test.go
+++ medium/2657_find_the_prefix_common_array_of_two_arrays/2657_find_the_prefix_common_array_of_two_arrays_test.go
@@ -1,1 +1,39 @@
 package find_the_prefix_common_array_of_two_arrays
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestFindThePrefixCommonArray(t *testing.T) {
+	tests := []struct {
+		name	string
+		A	[]int
+		B	[]int 
+		expectedResult []int 
+	} {
+		{
+			name: "Test Case 1",
+			A : []int{1,3,2,4},
+			B : []int{3,1,2,4},
+			expectedResult: []int{0,2,3,4},
+		},
+		{
+			name: "Test Case 2",
+			A : []int{2,3,1},
+			B : []int{3,1,2},
+			expectedResult: []int{0,1,3},
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			got := findThePrefixCommonArray(test.A, test.B)
+
+			if !reflect.DeepEqual(got, test.expectedResult) {
+				t.Errorf("findThePrefixCommonArray(%v, %v) = %v; want = %v", test.A, test.B, got, test.expectedResult)
+			}
+		})
+	}
+}
+	
+	
