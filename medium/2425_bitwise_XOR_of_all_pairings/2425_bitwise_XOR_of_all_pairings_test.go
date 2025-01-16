4273440ebb7e5ba16038a147a800001dc62b7401
LuaanNguyen <luanthiennguyen2003@gmail.com>
2025-01-16 11:48:36 -0700

    Complete daily 2425. Bitwise XOR of All Pairings

--- medium/2425_bitwise_XOR_of_all_pairings/2425_bitwise_XOR_of_all_pairings_test.go
+++ medium/2425_bitwise_XOR_of_all_pairings/2425_bitwise_XOR_of_all_pairings_test.go
@@ -1,1 +1,35 @@
 package bitwise_XOR_of_all_pairings
+
+import "testing"
+
+func TestXorAllNums(t *testing.T) {
+	tests := []struct {
+		name	string 
+		nums1	[]int
+		nums2	[]int
+		expectedResult	int
+	} {
+		{
+			name: "Test Case 1",
+			nums1: []int{2,1,3},
+			nums2: []int{10,2,5,0},
+			expectedResult: 13,
+		},
+		{
+			name: "Test Case 2",
+			nums1: []int{1,2},
+			nums2: []int{3,4},
+			expectedResult: 0,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T){
+			got := xorAllNums(test.nums1, test.nums2)
+
+			if got != test.expectedResult {
+				t.Errorf("xorAllNums(%v, %v) = %d; want = %d", test.nums1, test.nums2, got, test.expectedResult)
+			}
+		})
+	}
+}
