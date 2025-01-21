37b05f872dea351b3e8146e13ae4111d414dd2b7
LuaanNguyen <luanthiennguyen2003@gmail.com>
2025-01-21 14:01:47 -0700

    complete daily 2017. Grid Game

--- medium/2017_grid_game/2017_grid_game_test.go
+++ medium/2017_grid_game/2017_grid_game_test.go
@@ -3,3 +3,45 @@ package grid_game
 import "testing"
 
 
+func TestGridGame(t *testing.T) {
+	tests := []struct {
+		name 	string
+		grid	[][]int
+		expectedResult	int
+	} {
+		{
+			name: "Test Case 1",
+			grid: [][]int {
+				{2,5,4},
+				{1,5,1},
+			},
+			expectedResult: 4,
+		},
+		{
+			name: "Test Case 2",
+			grid: [][]int {
+				{3,3,1},
+				{8,5,2},
+			},
+			expectedResult: 4,
+		},
+		{
+			name: "Test Case 3",
+			grid: [][]int {
+				{1,3,1,15},
+				{1,3,3,1},
+			},
+			expectedResult: 7,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func (t *testing.T) {
+			got := gridGame(test.grid)
+			if got != test.expectedResult {
+				t.Errorf("gridGame(%v) = %d; want = %d", test.grid, got, test.expectedResult)
+			}
+		})
+	}
+
+}
